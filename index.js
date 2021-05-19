7090b23d000279314e01ae045b4068a3362ee0de
Corey Fuller <coreyfuller00@gmail.com>
2021-05-19 12:35:48 -0400

    Adding endpoints

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
+app.get('/messages', (req, res) => {
+    res.send("<h1>recieved</h1>")
+})
+
+app.get('/authenticate/', (req, res) => {
+    res.send("<h1>recieved</h1>")
+})
+
+app.get('/loads', (req, res) => {
+    res.send("<h1>recieved</h1>")
+
+})
+
+app.post('/messages', (req, res) => {
+    res.send("<h1>recieved</h1>")
+})
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
