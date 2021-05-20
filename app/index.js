4806dea47af9c3ccd08cdf0610a1a57ac671f4dc
Corey Fuller <coreyfuller00@gmail.com>
2021-05-20 14:13:53 -0400

    config database

--- app/index.js
+++ app/index.js
@@ -2,10 +2,29 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-// use the express-static middleware
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM app_user');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 app.use(express.static(path.join(__dirname,"/public")))
 
-// define the first route
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
@@ -21,8 +40,14 @@ app.get('/authenticate/:token', (req, res) => {
 })
 
 app.get('/loads', (req, res) => {
-    const response = require('./responses/loads.json')
-    res.send(response)
+    if(!req.headers.authorization){
+        res.send(401, 'Unauthorized due to missing or invalid token and/or API key.')
+
+    }
+    else {
+        const response = require('./responses/loads.json')
+        res.send(response)
+    }
 })
 
 app.put('/messages', (req, res) => {
